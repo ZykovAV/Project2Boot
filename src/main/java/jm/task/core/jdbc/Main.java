550fce7bbe4429a867aec4c06196518eefde8518
ZykovAV <cah34ka@gmail.com>
2022-08-11 00:00:12 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,12 @@ package jm.task.core.jdbc;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loading success!");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         // реализуйте алгоритм здесь
     }
 }
