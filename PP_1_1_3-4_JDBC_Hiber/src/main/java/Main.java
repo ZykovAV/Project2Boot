1f9e5f04756ff02e696b89c1f5026d178f68d480
ZykovAV <cah34ka@gmail.com>
2022-09-08 22:56:12 +0300

    3_1_1 ready

--- PP_1_1_3-4_JDBC_Hiber/src/main/java/Main.java
+++ PP_1_1_3-4_JDBC_Hiber/src/main/java/Main.java
@@ -29,19 +29,19 @@ public class Main {
 
     public static void main(final String[] args) throws Exception {
         final Session session = getSession();
-        try {
-            System.out.println("querying all the managed entities...");
-            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-            for (EntityType<?> entityType : metamodel.getEntities()) {
-                final String entityName = entityType.getName();
-                final Query query = session.createQuery("from " + entityName);
-                System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list()) {
-                    System.out.println("  " + o);
-                }
-            }
-        } finally {
-            session.close();
-        }
+//        try {
+//            System.out.println("querying all the managed entities...");
+//            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+//            for (EntityType<?> entityType : metamodel.getEntities()) {
+//                final String entityName = entityType.getName();
+//                final Query query = session.createQuery("from " + entityName);
+//                System.out.println("executing: " + query.getQueryString());
+//                for (Object o : query.list()) {
+//                    System.out.println("  " + o);
+//                }
+//            }
+//        } finally {
+//            session.close();
+//        }
     }
 }
