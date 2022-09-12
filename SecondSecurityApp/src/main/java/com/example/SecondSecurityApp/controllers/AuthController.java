6b181beaa51481294cd2894c97d2e30b02e99849
ZykovAV <cah34ka@gmail.com>
2022-09-12 22:48:32 +0300

    +custom html(realyze Login.html)

---
+++ SecondSecurityApp/src/main/java/com/example/SecondSecurityApp/controllers/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.SecondSecurityApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping
+    public String loginPage() {
+        return "auth/login";
+    }
+
+
+
+}
