581012cd23bd085b71eed75b509690ab61f1cf61
ZykovAV <cah34ka@gmail.com>
2022-09-12 22:37:55 +0300

    Authentication II

--- SecondSecurityApp/src/main/java/com/example/SecondSecurityApp/config/SecurityConfig.java
+++ SecondSecurityApp/src/main/java/com/example/SecondSecurityApp/config/SecurityConfig.java
@@ -1,26 +1,31 @@
 package com.example.SecondSecurityApp.config;
 
-import com.example.SecondSecurityApp.security.authProviderImpl;
+import com.example.SecondSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final authProviderImpl authProvider;
+    private  final PersonDetailsService personDetailsService;
 
-    @Autowired
-
-    public SecurityConfig(authProviderImpl authProvider) {
-        this.authProvider = authProvider;
+     @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
-
 
     //    nastroika autentifikacii
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
-//        super.configure(auth);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+         return NoOpPasswordEncoder.getInstance();
     }
 }
